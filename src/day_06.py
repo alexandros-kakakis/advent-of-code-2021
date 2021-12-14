2d7add485dd5ec6b0263eff7e4f875d862d75239
Alexandros Kakakis <alexandroskakakis@gmail.com>
2021-12-14 16:59:30 +0100

    Simplify solution day 6

--- src/day_06.py
+++ src/day_06.py
@@ -1,71 +1,38 @@
 from helper_functions import read_input_as_list
-import numpy as np
+from collections import Counter
 
 puzzle_input = read_input_as_list('./data/input_06.txt')
 puzzle_input = puzzle_input[0].split(',')
-puzzle_input = [ int(i) for i in puzzle_input ]
-puzzle_input = np.array( puzzle_input )
 
-class Fish:
-    def __init__(self, age, n_days_left, knowledge):
-        self.age = age
-        self.n_children = 0
-        self.n_days_left = n_days_left
-        self.knowledge = knowledge
-        
-    def calculate_n_children(self):
-        if self.age >= self.n_days_left:
-            pass
+def convert_puzzle_input_to_fish(puzzle_input):
+    fish = { i: 0 for i in range(9) }
 
-        elif ( self.age == 8 ) & ( self.knowledge[self.n_days_left] >= 0 ):
-            self.n_children += self.knowledge[self.n_days_left]
-        
-        else:
-            self.n_days_left -= ( self.age + 1 )
-            self.n_children += 1
+    for i in puzzle_input:
+        fish[int(i)] += 1
 
-            child = Fish(age=8, n_days_left=self.n_days_left, knowledge=self.knowledge)
-            child.calculate_n_children()
-
-            self.n_children += child.n_children        
-
-            while self.n_days_left >= 7:
-                self.n_days_left -= 7
-                self.n_children += 1
-
-                child = Fish(age=8, n_days_left=self.n_days_left, knowledge=self.knowledge)
-
-                child.calculate_n_children()
-                self.knowledge[self.n_days_left] = child.n_children
-
-                self.n_children += child.n_children
+    return fish
     
+def wait_n_days(n_days):
+    global fish 
 
-def calculate_number_of_fish(puzzle_input, n_days):
-    total_fish = 0
-
-    unique_ages = np.unique( puzzle_input )
-    knowledge = np.ones( n_days + 1 ) * (-1)
-
-    for age in unique_ages:   
-        total_fish_age = 0
-
-        fish = Fish(age=age, n_days_left=n_days, knowledge=knowledge)
-
-        fish.calculate_n_children()
-        knowledge = fish.knowledge
-
-        total_fish_age += fish.n_children
-        total_fish_age += 1
-
-        total_fish += np.sum( puzzle_input == age ) * total_fish_age
-
-    return int(total_fish)
-
+    for _ in range(n_days):
+        for age, count in fish.copy().items():
+            if age == 0:
+                fish[age] = 0
+                fish[6] += count
+                fish[8] += count
+            else:        
+                fish[age] -= count
+                fish[age-1] += count
+            
 ## Part One
-n_fish = calculate_number_of_fish(puzzle_input, n_days=80)
+fish = convert_puzzle_input_to_fish(puzzle_input)
+wait_n_days(n_days=80)
+n_fish = sum( fish.values() )
 print('Part One:', n_fish)
 
 ## Part Two
-n_fish = calculate_number_of_fish(puzzle_input, n_days=256)
+fish = convert_puzzle_input_to_fish(puzzle_input)
+wait_n_days(n_days=256)
+n_fish = sum( fish.values() )
 print('Part Two:', n_fish)
